ad42dc4bd2e6091cfb88b69c368e8ef5bb543197
simoneburrai <burrai.s98@gmail.com>
2025-03-05 13:38:36 +0100

    feat: folder structure created

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// // Dichiarazione e inizializzazione Variabili
+// let kilometers = parseFloat(prompt("Inserire il numero di chilometri da percorrere"));
+// if (isNaN(kilometers)){
+//     alert("Inserire un valore numerico");
+//     exit();
+// }
+// const age = parseFloat(prompt("Inserire la propria età"));
+// if (isNaN(age)){
+//     alert("Inserire un valore numerico");
+//     exit();
+// }
+// const pricePerKilometer = 0.21;
+
+// let ticketPrice = kilometers * pricePerKilometer;
+
+// // Condizioni per lo sconto 
+// if (age < 18) {
+//     ticketPrice = ticketPrice - ((20 * ticketPrice)/ 100);
+//     alert("Avete ottenuto lo sconto del 20%");
+// } else if (age >= 65) {
+//     ticketPrice = ticketPrice - ((40 * ticketPrice)/ 100);
+//     alert("Avete ottenuto lo sconto del 40%");
+// }
+
+// // Arrotondamento a 2 cifre decimali del prezzo del biglietto 
+// ticketPrice = ticketPrice.toFixed(2);
+// // Stampa dei dati acquistiti (Chilometri da percorrere e età del passeggero)
+// // Stampa del prezzo effettivo del biglietto 
+// alert(`I chilometri che dovrete percorrere sono ${kilometers}
+//     La vostra età è di ${age} anni
+//     Il prezzo del vostro biglietto è di ${ticketPrice} €`);
